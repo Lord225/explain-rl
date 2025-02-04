f402283a4ddd66b3b63eabaf346672d5ecae4504
Lord225 <zlotymaciej@gmail.com>
2025-02-04 01:10:20 +0100

    .

---
+++ env-test.py
@@ -0,0 +1,19 @@
+
+from gym3 import types_np
+from procgen import ProcgenGym3Env
+import matplotlib.pyplot as plt
+
+# human rendering
+env = ProcgenGym3Env(num=1, env_name="coinrun", distribution_mode="easy", num_levels=0, start_level=0, render_mode="rgb_array")
+step = 0
+while True:
+    env.act(types_np.sample(env.ac_space, bshape=(env.num,)))
+    rew, obs, first = env.observe()
+    print(obs['rgb'].shape)
+    plt.imshow(obs['rgb'][0])
+    plt.show()
+    
+    print(f"step {step} reward {rew} first {first}")
+    if step > 0 and first:
+        break
+    step += 1
